bee4ab9c48cc799b819aba9551fe4d31045a4c1f
U-UCEBNE\drahos <drahos@D6.(none)>
2010-10-25 18:36:18 +0200

    Update now references the repo directly

--- update.bat
+++ update.bat
@@ -5,6 +5,7 @@ echo This simple script will update the distribution. All tracked files will be 
 setlocal
 set PATH=%~dp0\toolchain\bin;%PATH%
 git reset --hard HEAD
-git fetch
-git merge -s recursive -Xtheirs FETCH_HEAD
+git fetch git://github.com/drahosp/PaPP.git HEAD
+git merge -s ours FETCH_HEAD
 endlocal
+
