83add0c2d201b682462f25f2b6de7ea86b068373
Peter Drahoš <drahosp@gmail.com>
2011-10-16 15:21:36 +0200

    Minor compatibility modification for usleep and sleep on MinGW.

--- toolchain/include/unistd.h
+++ toolchain/include/unistd.h
@@ -30,15 +30,18 @@
 extern "C" {
 #endif
 
-// Modifikacia koli kompatibilite
-// sleep zial nieje v MinGW, Pouzijeme Sleep z win32 API (drahosp)
+// Compatibility modification
+// sleep and usleep in MinGW are not defined by default
+// the following macros make use of the Windows Sleep function instead (drahosp)
 #include <Windows.h>
 #define sleep(x) Sleep((x)*1000)
 
 #if !defined __NO_ISOCEXT
 #include <sys/types.h> /* For useconds_t. */
 
-int __cdecl __MINGW_NOTHROW usleep(useconds_t useconds);
+// Using Windows Sleep instead of the built in inplementation
+#define usleep(x) Sleep((x)/1000)
+//int __cdecl __MINGW_NOTHROW usleep(useconds_t useconds);
 #endif  /* Not __NO_ISOCEXT */
 
 /* This is defined as a real library function to allow autoconf
